c4b36671d4ed776eb0b2224f2458027f5d03e598
Sim Dong Min <codongmin@gmail.com>
2025-02-24 14:44:49 +0900

    Time: 22 ms (22.48%), Space: 55.6 MB (89.93%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        HashMap<Integer, HashSet<Integer>> uam = new HashMap<>();
+        int[] answer = new int[k];
+
+        for (int[] log : logs) {
+            int userId = log[0];
+            int minute = log[1];
+
+            if (!uam.containsKey(userId)) {
+                uam.put(userId, new HashSet<>());
+                uam.get(userId).add(minute);
+            } else {
+                uam.get(userId).add(minute);
+            }
+        }
+
+        for (Integer userId : uam.keySet()) {
+            HashSet<Integer> set = uam.get(userId);
+            answer[set.size()-1]++;
+        }
+
+        return answer;
+    }
+}
