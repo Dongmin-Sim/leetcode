f2c2bd1d308b562aa36f239cedbd3e658bacbc6e
Sim Dong Min <codongmin@gmail.com>
2025-04-28 18:55:00 +0900

    Time: 4 ms (78.19%), Space: 97.3 MB (23.4%) - LeetHub

--- 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.java
+++ 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public long countCompleteDayPairs(int[] hours) {
         long result = 0;
-        int[] remainHours = new int[24];
+        int[] count = new int[24];
 
         for (int hour : hours) {
-            result += remainHours[(24 - hour % 24) % 24];
-            remainHours[hour % 24]++;
+            result += count[(24 - hour % 24) % 24];
+            count[hour % 24]++;
         }
         return result;
     }
