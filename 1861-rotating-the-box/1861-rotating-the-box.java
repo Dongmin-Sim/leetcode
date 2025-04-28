9450aebf9f12e9ba9601912638d9e39dd875955f
Sim Dong Min <codongmin@gmail.com>
2025-04-28 18:48:42 +0900

    Time: 9 ms (41.99%), Space: 79.9 MB (23.19%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -3,36 +3,31 @@ class Solution {
         int m = boxGrid.length;
         int n = boxGrid[0].length;
 
-        for (int i = 0; i < m; i++) {
-            int curPoint = n - 1;
-
-            while (curPoint > 0) {
-                if (boxGrid[i][curPoint] == '.') {
-                    for (int nextPoint = curPoint - 1; nextPoint >= 0; nextPoint--) {
-                        if (boxGrid[i][nextPoint] == '*') break;
-                        if (boxGrid[i][nextPoint] == '#') {
-                            boxGrid[i][curPoint] = '#';
-                            boxGrid[i][nextPoint] = '.';
-                            break;
-                        }
-                    }
+                for (int i = 0; i < m; i++) {
+            int pointer = n-1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    pointer = j - 1;
+                } else if (boxGrid[i][j] == '#') {
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][pointer] = '#';
+                    pointer--;
                 }
-                curPoint--;
             }
         }
 
-        return rotateBox(m, n, boxGrid);
+        return rotateClockwise(m, n, boxGrid);
     }
 
-    private char[][] rotateBox(int m, int n, char[][] boxGrid) {
-        char[][] rotatedBox = new char[n][m];
+    private char[][] rotateClockwise(int m, int n, char[][] boxGrid) {
+        char[][] newGrid = new char[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                rotatedBox[i][j] = boxGrid[m-j-1][i];
+                newGrid[i][j] = boxGrid[m - j - 1][i];
             }
         }
 
-        return rotatedBox;
+        return newGrid;
     }
 }
