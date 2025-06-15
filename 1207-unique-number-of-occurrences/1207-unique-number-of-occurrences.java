53ace7a2ca462ae10c93345317dae507f22f72f0
Sim Dong Min <codongmin@gmail.com>
2025-06-15 17:23:37 +0900

    Time: 2 ms (98.71%), Space: 42.1 MB (45.15%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] table = new int[1001];
 
         for (int num : arr) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (Integer key : map.keySet()) {
-            Integer val = map.get(key);
-            if (table[val] != 0) return false;
-            table[val]++;
-        }
+        HashSet<Integer> countUnique = new HashSet<>();
+        for (Integer value : map.values()) {
+            if (countUnique.contains(value)) {
+                return false;
+            }
 
+            countUnique.add(value);
+        }
         return true;
     }
 }
